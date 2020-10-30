3d75e802d4e0e9b65d13e22da57b4859cd376553
Matteo Boffo aka stempelo <matteo.boffo@gmail.com>
2020-10-30 11:44:50 +0100

    add

--- chocolatey_installazione_pacchetti.ps1
+++ chocolatey_installazione_pacchetti.ps1
@@ -9,3 +9,4 @@ choco install xml-notepad               #editor xml
 
 #Framework/Ambienti
 choco install nodejs.install
+choco install meteor
--- chocolatey_upgrade_pacchetti.ps1
+++ chocolatey_upgrade_pacchetti.ps1
@@ -2,7 +2,10 @@
 #Powershell come amministratore
 
 choco upgrade chocolatey
+
 choco upgrade notepadplusplus.install
 choco upgrade xml-notepad
 choco upgrade pspad
+
 choco upgrade nodejs.install
+choco upgrade meteor
