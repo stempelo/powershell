a11022d41743c31f9e85969b4c5bd0cea5369f1f
Matteo Boffo aka stempelo <matteo.boffo@gmail.com>
2021-03-20 09:02:12 +0100

    Update chocolatey_upgrade_pacchetti.ps1
    Comment

--- chocolatey_upgrade_pacchetti.ps1
+++ chocolatey_upgrade_pacchetti.ps1
@@ -3,9 +3,11 @@
 
 choco upgrade chocolatey
 
+#Editor
 choco upgrade notepadplusplus.install
 choco upgrade xml-notepad
 choco upgrade pspad
 
+#Framework/Ambienti
 choco upgrade nodejs.install
 choco upgrade meteor
